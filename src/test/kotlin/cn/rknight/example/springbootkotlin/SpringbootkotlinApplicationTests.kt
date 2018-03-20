c53c584095ceac2e9318a4f1b0639fe376089dcd
shanhongqiang <shanhongqiang@jd.com>
2018-03-20 16:08:31 +0800

    first commit

---
+++ src/test/kotlin/cn/rknight/example/springbootkotlin/SpringbootkotlinApplicationTests.kt
@@ -0,0 +1,16 @@
+package cn.rknight.example.springbootkotlin
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringbootkotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
